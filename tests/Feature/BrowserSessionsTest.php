fcfdaad2538e5d6d494b9acc0b2e61d1ff186c07
subinkhang <subinkhang102@gmail.com>
2024-04-20 09:26:26 +0700

    php install

--- tests/Feature/BrowserSessionsTest.php
+++ tests/Feature/BrowserSessionsTest.php
@@ -1,14 +1,24 @@
 <?php
 
+namespace Tests\Feature;
+
 use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Jetstream\Http\Livewire\LogoutOtherBrowserSessionsForm;
 use Livewire\Livewire;
+use Tests\TestCase;
 
-test('other browser sessions can be logged out', function () {
-    $this->actingAs($user = User::factory()->create());
+class BrowserSessionsTest extends TestCase
+{
+    use RefreshDatabase;
 
-    Livewire::test(LogoutOtherBrowserSessionsForm::class)
-        ->set('password', 'password')
-        ->call('logoutOtherBrowserSessions')
-        ->assertSuccessful();
-});
+    public function test_other_browser_sessions_can_be_logged_out(): void
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        Livewire::test(LogoutOtherBrowserSessionsForm::class)
+            ->set('password', 'password')
+            ->call('logoutOtherBrowserSessions')
+            ->assertSuccessful();
+    }
+}
