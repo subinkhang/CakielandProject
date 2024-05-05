8e3ea3914a7687c6c8874941a4748faa8981158d
hoangloc84 <hoangloc084@gmail.com>
2024-05-05 22:39:37 +0700

    Auto stash before checking out "origin/addAdminSide"

--- resources/views/admin/adminAddProduct.blade.php
+++ resources/views/admin/adminAddProduct.blade.php
@@ -18,6 +18,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <!-- Custom CSS -->
 <link href="{{asset('backend/css/style.css')}}" rel='stylesheet' type='text/css' />
 <link href="{{asset('backend/css/style-responsive.css')}}" rel="stylesheet"/>
+<link href="{{asset('backend/css/adminaddproduct.css')}}" rel="stylesheet"/>
 <!-- font CSS -->
 <link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 <!-- font-awesome icons -->
@@ -351,117 +352,107 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 </aside>
 <!--sidebar end-->
 <!--main content start-->
+
+
+<!------------------------ALTER----------------------------->
+<!--------------------LEFT--------------------------------->
 <section id="main-content">
 	<section class="wrapper">
 		<!-- page start-->
 		<div class="mail-w3agile">
         <div class="row">
-            <div class="col-sm-3 com-w3ls">
+            <div class="col-sm-8 com-w3ls">
                 <section class="panel">
-                    <div class="panel-body">
-                        <a href="mail_compose.html"  class="btn btn-compose">
-                            Compose Mail
-                        </a>
-                        <ul class="nav nav-pills nav-stacked mail-nav">
-                            <li class="active"><a href="mail.html"> <i class="fa fa-inbox"></i> Inbox  <span class="label label-danger pull-right inbox-notification">9</span></a></li>
-                            <li><a href="#"> <i class="fa fa-envelope-o"></i> Send Mail</a></li>
-                            <li><a href="#"> <i class="fa fa-certificate"></i> Important</a></li>
-                            <li><a href="#"> <i class="fa fa-file-text-o"></i> Drafts <span class="label label-info pull-right inbox-notification">123</span></a></a></li>
-                            <li><a href="#"> <i class="fa fa-trash-o"></i> Trash</a></li>
-                        </ul>
-                    </div>
+                    <h4 class="menu"id="text-name"><b>Name</b></h4>
+                    <textarea type="text" id="name" class="text-title"></textarea>
+                    <h4 class="menu"><b>Current Price</b></h4>
+                    <textarea type="text" id="curp" class="text-title"></textarea>
+                    <h4 class="menu"><b>New Price</b></h4>
+                    <textarea type="text" id="newp" class="text-title"></textarea>
+                    <h4 class="menu"><b>Brand</b></h4>
+                    <textarea type="text" id="brand" class="text-title"></textarea>
+                    <h4 class="menu"><b>Description</b></h4>
+                    <textarea type="text" id="desc" class="text-title"></textarea>
+                    <h4 class="menu"><b>Detail</b></h4>
+                    <textarea type="text" id="detail"  class="text-title"></textarea>
+                    <h4 class="menu"><b>Technical</b></h4>
+                    <textarea type="text" id="tech" class="text-title"></textarea>
+                    <div class="col-8 bt-pay pm">
+                    <button class="btn" id="btn-p">
+                        <p1>Update</p1>
+                    </button>
+                </div>
                 </section>
 
-                <section class="panel">
-                    <div class="panel-body">
-                        <ul class="nav nav-pills nav-stacked labels-info ">
-                            <li> <h4>Buddy online</h4> </li>
-                            <li> <a href="#"> <i class="fa fa-comments-o text-success"></i> Jonathan Smith <p>I do not think</p></a>  </li>
-                            <li> <a href="#"> <i class="fa fa-comments-o text-danger"></i> iRon <p>Busy with coding</p></a> </li>
-                            <li> <a href="#"> <i class="fa fa-comments-o text-muted "></i> Anjelina Joli <p>I out of control</p></a></li>
-                            <li> <a href="#"> <i class="fa fa-comments-o text-muted "></i> Samual Daren <p>I am not here</p></a></li>
-                            <li> <a href="#"> <i class="fa fa-comments-o text-muted "></i> Tis man <p>I do not think</p></a>  </li>
-                        </ul>
-                        <a href="#"> + Add More</a>
-
-                        <div class="inbox-body text-center inbox-action">
-                            <div class="btn-group">
-                                <a class="btn mini btn-default" href="javascript:;">
-                                    <i class="fa fa-power-off"></i>
-                                </a>
-                            </div>
-                            <div class="btn-group">
-                                <a class="btn mini btn-default" href="javascript:;">
-                                    <i class="fa fa-cog"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+<!-----------------------------------------------RIGHT---------------------------------------------------------------------------->
             </div>
-            <div class="col-sm-9 mail-w3agile">
+            <div class="col-sm-3 mail-w3agile">
                 <section class="panel">
-                    <header class="panel-heading wht-bg">
-                       <h4 class="gen-case"> Compose Mail
-                           <form action="#" class="pull-right mail-src-position">
-                            <div class="input-append">
-                                <input type="text" class="form-control " placeholder="Search Mail">
-                            </div>
-                        </form>
-                       </h4>
-                    </header>
-                    <div class="panel-body">
-                        <div class="compose-btn pull-right">
-                            <button class="btn btn-primary btn-sm"><i class="fa fa-check"></i> Send</button>
-                            <button class="btn btn-sm"><i class="fa fa-times"></i> Discard</button>
-                            <button class="btn btn-sm">Draft</button>
-                        </div>
-                        <div class="compose-mail">
-                            <form role="form-horizontal" method="post">
-                                <div class="form-group">
-                                    <label for="to" class="">To:</label>
-                                    <input type="text" tabindex="1" id="to" class="form-control">
-
-                                    <div class="compose-options">
-                                        <a onclick="$(this).hide(); $('#cc').parent().removeClass('hidden'); $('#cc').focus();" href="javascript:;">Cc</a>
-                                        <a onclick="$(this).hide(); $('#bcc').parent().removeClass('hidden'); $('#bcc').focus();" href="javascript:;">Bcc</a>
-                                    </div>
-                                </div>
-
-                                <div class="form-group hidden">
-                                    <label for="cc" class="">Cc:</label>
-                                    <input type="text" tabindex="2" id="cc" class="form-control">
-                                </div>
-
-                                <div class="form-group hidden">
-                                    <label for="bcc" class="">Bcc:</label>
-                                    <input type="text" tabindex="2" id="bcc" class="form-control">
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="subject" class="">Subject:</label>
-                                    <input type="text" tabindex="1" id="subject" class="form-control">
-                                </div>
-
-                                <div class="compose-editor">
-                                    <textarea class="wysihtml5 form-control" rows="9"></textarea>
-                                    <input type="file" class="default">
-                                </div>
-                                <div class="compose-btn">
-                                    <button class="btn btn-primary btn-sm"><i class="fa fa-check"></i> Send</button>
-                                    <button class="btn btn-sm"><i class="fa fa-times"></i> Discard</button>
-                                    <button class="btn btn-sm">Draft</button>
-                                </div>
-
-                            </form>
-                        </div>
+                    <div class="right">
+                    <h4 class="right-text"><b>Image</b></h4>
+                    <input type="file" class="image" accept="image/png, image/jpeg" multiple>
+                    <div id="main-img"></div>
+                    <h4  class="right-text"><b>Galary</b></h4>
+                    <input type="file" class="image" accept="image/png, image/jpeg" multiple>
+                    <div class="cont">
+                    <div class="gal"></div>
+                    <div class="gal"></div>
+                    <div class="gal"></div>
+                    <div class="gal"></div>
+                    <div class="gal"></div>
+                    <div class="gal"></div>
                     </div>
+                    <h4 class="right-text"><b>Color</b></h4>
+                    <div class="cont">
+                    <div class="grey">
+                    <input type="checkbox" style="text-align: center" id="op1">
+                    <div style="background-color: grey" class="color"></div>
+                    </div>
+                    <div class="black">
+                    <input type="checkbox" style="text-align: center" id="op2">
+                    <div style="background-color: black" class="color"></div>
+                    </div>
+                    <div class="white">
+                    <input type="checkbox" style="text-align: center" id="op3">
+                    <div style="background-color: white" class="color"></div>
+                    </div>
+                    
+                    </div>
+                    <h4 class="right-text"><b>Menu</b></h4>
+                    <select id="cate" style="margin-top: 10px, width: 195px; border-radius: 15px;">
+                        <option value="">Categories</option>
+                        <option value="wet">Wet Indredients</option>
+                        <option value="dry">Dry Indredients</option>
+                        <option value="baking">Baking Tools</option>
+                        <option value="cooking">Utensiles</option>
+                        <option value="bartool">Bar Tool</option>
+                        <option value="bar">Bar Ingredients</option>
+                    </select>
+                    <h4 class="right-text"><b>Tag</b></h4>
+                    <select id="tag" style="margin-top: 10px, width: 195px; border-radius: 15px; margin-bottom: 30px;">
+                    <option value="">Tag</option>
+                        <option value="milk">Milk</option>
+                        <option value="butter">Butter</option>
+                        <option value="flour">Flour</option>
+                        <option value="bakingsoda">Baking Soda</option>
+                        <option value="egg">Eggs Beater</option>
+                        <option value="mold">Mold</option>
+                        <option value="potset">Stainless Steel Pot Set</option>
+                        <option value="knifeset">Kitchen Knife Set</option>
+                        <option value="foammaker">Coffee Foam Maker</option>
+                        <option value="cup">Measuring Cup</option>
+                        <option value="tea">Tea</option>
+                        <option value="syrup">Syrup</option>
+                    </div>
+                    
                 </section>
             </div>
         </div>
+        
 
         <!-- page end-->
 		 </div>
+         
 </section>
  <!-- footer -->
 		  <div class="footer">
@@ -481,5 +472,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <script src="{{asset('backend/js/jquery.nicescroll.js')}}"></script>
 <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/flot-chart/excanvas.min.js"></script><![endif]-->
 <script src="{{asset('backend/js/jquery.scrollTo.js')}}"></script>
+<script src="{{asset('backend/js/adminaddproduct.js')}}"></script>
 </body>
 </html>
