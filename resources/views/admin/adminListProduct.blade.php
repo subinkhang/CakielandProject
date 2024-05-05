41d127364dbc157568a237da2ab13dded1e2aae1
trang <nhongtrang878@gmail.com>
2024-05-05 19:28:03 +0700

    adminproductlist

--- resources/views/admin/adminListProduct.blade.php
+++ resources/views/admin/adminListProduct.blade.php
@@ -25,6 +25,8 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <link href="{{asset('backend/css/font-awesome.css')}}" rel="stylesheet"> 
 <!-- //font-awesome icons -->
 <script src="{{asset('backend/js/jquery2.0.3.min.js')}}"></script>
+<link rel="stylesheet" href="{{('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css')}}">
+<link rel="stylesheet" href="{{asset('backend/css/admin-list-product.css')}}">
 </head>
 <body>
 <section id="container">
@@ -351,14 +353,15 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 </aside>
 <!--sidebar end-->
 <!--main content start-->
+<!-- làm từ đây -->
 <section id="main-content">
 	<section class="wrapper">
-		<div class="table-agile-info">
+		<div class="table-agile-info" id="background">
   <div class="panel panel-default">
-    <div class="panel-heading">
-      Responsive Table
+    <div class="panel-heading" id="heading">
+      Product List
     </div>
-    <div class="row w3-res-tb">
+    <!-- <div class="row w3-res-tb">
       <div class="col-sm-5 m-b-xs">
         <select class="input-sm form-control w-sm inline v-middle">
           <option value="0">Bulk action</option>
@@ -378,93 +381,128 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
           </span>
         </div>
       </div>
-    </div>
+    </div> -->
     <div class="table-responsive">
       <table class="table table-striped b-t b-light">
         <thead>
           <tr>
-            <th style="width:20px;">
+            <!-- <th style="width:20px;">
               <label class="i-checks m-b-none">
-                <input type="checkbox"><i></i>
+              <input type="checkbox"><i></i>
               </label>
-            </th>
-            <th>Project</th>
-            <th>Task</th>
+            </th> -->
+            <!-- <th></th> -->
+            <th>Id</th>
+            <th>Product's name</th>
+            <th>Price</th>
             <th>Date</th>
-            <th style="width:30px;"></th>
+            <th><input type="submit" class="btn_addpro" value="Add"></th>
+            <!-- <th style="width:30px;"></th> -->
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td>
-            <td>Idrawfast prototype design prototype design prototype design prototype design prototype design</td>
-            <td><span class="text-ellipsis">{item.PrHelpText1}</span></td>
-            <td><span class="text-ellipsis">{item.PrHelpText1}</span></td>
+            <!-- <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td> -->
+            <td>01</td>
+            <td>Philips HR3705 Egg Beater (300W)</td>
+            <td><span class="text-ellipsis">$100.00</span></td>
+            <td><span class="text-ellipsis">Jul 22, 2013</span></td>
             <td>
-              <a href="" class="active" ui-toggle-class=""><i class="fa fa-check text-success text-active"></i><i class="fa fa-times text-danger text"></i></a>
+                <a href="" class="active" ui-toggle-class="" id="icon">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                    <i class="fa-regular fa-trash-can trash-icon" onclick="deleteProduct()" style="margin-left: 20px;"></i>
+                </a>
             </td>
           </tr>
           <tr>
-            <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td>
+            <!-- <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td> -->
+            <td>02</td>
             <td>Formasa</td>
-            <td>8c</td>
+            <td>$200.00</td>
             <td>Jul 22, 2013</td>
             <td>
-              <a href="" ui-toggle-class=""><i class="fa fa-check text-success text-active"></i><i class="fa fa-times text-danger text"></i></a>
+                <a href="" class="active" ui-toggle-class="" id="icon">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                    <i class="fa-regular fa-trash-can" style="margin-left: 20px;"></i>
+                </a>
             </td>
           </tr>
           <tr>
-            <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td>
+            <!-- <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td> -->
+            <td>03</td>
             <td>Avatar system</td>
-            <td>15c</td>
+            <td>$150.00</td>
             <td>Jul 15, 2013</td>
             <td>
-              <a href="" class="active" ui-toggle-class=""><i class="fa fa-check text-success text-active"></i><i class="fa fa-times text-danger text"></i></a>
+                <a href="" class="active" ui-toggle-class="" id="icon">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                    <i class="fa-regular fa-trash-can" style="margin-left: 20px;"></i>
+                </a>
             </td>
           </tr>
           <tr>
-            <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td>
+            <!-- <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td> -->
+            <td>04</td>
             <td>Throwdown</td>
-            <td>4c</td>
+            <td>$160.00</td>
             <td>Jul 11, 2013</td>
             <td>
-              <a href="" class="active" ui-toggle-class=""><i class="fa fa-check text-success text-active"></i><i class="fa fa-times text-danger text"></i></a>
+                <a href="" class="active" ui-toggle-class="" id="icon">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                    <i class="fa-regular fa-trash-can" style="margin-left: 20px;"></i>
+                </a>
             </td>
           </tr>
           <tr>
-            <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td>
+            <!-- <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td> -->
+            <td>05</td>
             <td>Idrawfast</td>
-            <td>4c</td>
+            <td>$130.00</td>
             <td>Jul 7, 2013</td>
             <td>
-              <a href="" class="active" ui-toggle-class=""><i class="fa fa-check text-success text-active"></i><i class="fa fa-times text-danger text"></i></a>
+                <a href="" class="active" ui-toggle-class="" id="icon">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                    <i class="fa-regular fa-trash-can" style="margin-left: 20px;"></i>
+                </a>
             </td>
           </tr>
           <tr>
-            <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td>
+            <!-- <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td> -->
+            <td>06</td>
             <td>Formasa</td>
-            <td>8c</td>
+            <td>$123.00</td>
             <td>Jul 3, 2013</td>
             <td>
-              <a href="" class="active" ui-toggle-class=""><i class="fa fa-check text-success text-active"></i><i class="fa fa-times text-danger text"></i></a>
+                <a href="" class="active" ui-toggle-class="" id="icon">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                    <i class="fa-regular fa-trash-can" style="margin-left: 20px;"></i>
+                </a>
             </td>
           </tr>
           <tr>
-            <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td>
+            <!-- <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td> -->
+            <td>07</td>
             <td>Avatar system</td>
-            <td>15c</td>
+            <td>$147.08</td>
             <td>Jul 2, 2013</td>
             <td>
-              <a href="" class="active" ui-toggle-class=""><i class="fa fa-check text-success text-active"></i><i class="fa fa-times text-danger text"></i></a>
+                <a href="" class="active" ui-toggle-class="" id="icon">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                    <i class="fa-regular fa-trash-can" style="margin-left: 20px;"></i>
+                </a>
             </td>
           </tr>
           <tr>
-            <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td>
+            <!-- <td><label class="i-checks m-b-none"><input type="checkbox" name="post[]"><i></i></label></td> -->
+            <td>08</td>
             <td>Videodown</td>
-            <td>4c</td>
+            <td>$147.08</td>
             <td>Jul 1, 2013</td>
             <td>
-              <a href="" class="active" ui-toggle-class=""><i class="fa fa-check text-success text-active"></i><i class="fa fa-times text-danger text"></i></a>
+                <a href="" class="active" ui-toggle-class="" id="icon">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                    <i class="fa-regular fa-trash-can" style="margin-left: 20px;"></i>
+                </a>
             </td>
           </tr>
         </tbody>
@@ -474,7 +512,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
       <div class="row">
         
         <div class="col-sm-5 text-center">
-          <small class="text-muted inline m-t-sm m-b-sm">showing 20-30 of 50 items</small>
+          <small class="text-muted inline m-t-sm m-b-sm">showing 01-08 of 50 items</small>
         </div>
         <div class="col-sm-7 text-right text-center-xs">                
           <ul class="pagination pagination-sm m-t-none m-b-none">
@@ -509,5 +547,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <script src="{{asset('backend/js/jquery.nicescroll.js')}}"></script>
 <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/flot-chart/excanvas.min.js"></script><![endif]-->
 <script src="{{asset('backend/js/jquery.scrollTo.js')}}"></script>
+<script src="{{asset('backend/js/admin_list_product.js')}}"></script>
 </body>
 </html>
