1a170be47adbe2f2af7501e973f5b4abef8befd4
Gia Bách <vogiabach77@gmail.com>
2024-05-11 14:30:02 +0700

    04 Feature Product - Homepage

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -22,4 +22,21 @@ class HomepageController extends Controller
         DB::table('email_customer')->insert($data);
         return Redirect::to('/');
     }
+    public function getAllProducts()
+    {
+        // Lấy ra 8 sản phẩm có lượt bán nhiều nhất
+        $top_products = DB::table('order_detail')
+                        ->select('product_id', DB::raw('SUM(quantity) as total_quantity'))
+                        ->groupBy('product_id')
+                        ->orderByDesc('total_quantity')
+                        ->take(4)
+                        ->get();
+
+        // Lấy thông tin chi tiết của các sản phẩm
+        $product_ids = $top_products->pluck('product_id')->toArray();
+        $all_product = DB::table('product')->whereIn('id', $product_ids)->get();
+
+        // Truyền danh sách sản phẩm vào view
+        return view('homepage', ['all_product' => $all_product]);
+    }
 }
