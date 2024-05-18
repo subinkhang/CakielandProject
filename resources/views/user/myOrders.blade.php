1ab8780812cf8f70829ac8b0367aa458d06ae2b1
hanhuyen <daothihanhuyen10a3.19.20@gmail.com>
2024-05-18 20:23:13 +0700

    Update uderId Myorders

--- resources/views/user/myOrders.blade.php
+++ resources/views/user/myOrders.blade.php
@@ -1,7 +1,6 @@
 <x-app-layout>
     <title>{{ ucwords(str_replace('-', ' ', last(explode('/', url()->current())))) }}</title>
     <link rel="stylesheet" href="{{ asset('/frontend/css/myorder.css') }}">
-
     <livewire:breadcrumb-banner />
     <!-- ACCOUNT -->
     <div class="container my_orders_container">
@@ -23,73 +22,10 @@
                 </div>
             </div>
             <div class="col-9">
-                {{-- @if (count($data) > 0)
-                    <div class="row">
-                        @foreach ($data as $order)
-                        <div class="col-12 product_detail">
-                            <div class="col-12 order_title">
-                                <h5>{{ $order['status']}}</h5>
-                            </div>
-                            @foreach ($order['items'] as $item)
-                            <div class="row">
-                                <div class="col-3" class="img_order">
-                                    <img src="{{ asset('frontend/images/My-order/71012Ro-efL.jpg') }}" alt=""
-                                        class="w-100">
-                                </div>
-                                <div class="col-5 order_name">
-                                    <h5>{{ $item['product_name'] }}</h5>
-                                    <p> Quantity: {{ $item['quantity'] }}</p>
-                                </div>
-                                <div class="col-4">
-                                    <p class="price">{{ $item['price'] }}</p>
-                                </div>
-                                <div class="col-3 status">
-                                    <h5>Order status: </h5>
-                                </div>
-                                <div class="col-5 order_deli">
-                                    <h5>{{ $order['status'] }}</h5>
-                                </div>
-                                <div class="col-4 total_price">
-                                    <h6>Total: {{ $item['total_money'] }}</h6>
-                                    <button type="submit" class="button_order">Buy Again</button>
-                                </div>
-                            </div>
-                            @endforeach
-                        </div>
-                        @endforeach
-                    </div> 
-                    @endif  --}}
-                    
-                {{-- <div class="row">
-                    @foreach ($data as $item)
-                    <div class="col-12 product_detail_2">
-                        <div class="col-12 order_title">
-                            <h5>DELIVERED</h5>
-                        </div>
-                        <div class="row">
-                            <div class="col-3" class="img_order">
-                                <img src="{{ asset('frontend/images/My-order/71012Ro-efL.jpg') }}" alt=""
-                                    class="w-100">
-                            </div>
-                            <div class="col-5 order_name">
-                                <h5>Cook Classic Wood Rolling Pin</h5>
-                                <p>Quantity: 1</p>
-                            </div>
-                            <div class="col-4">
-                                <p class="price">$112.00</p>
-                            </div>
-                            <div class="col-3 status"></div>
-                            <div class="col-5 order_deli"></div>
-                            <div class="col-4 total_price">z
-                                <h6>Total: $112.00</h6>
-                                <button type="submit" class="button_order">Buy Again</button>
-                            </div>
-                        </div>
-                    </div>
-                    @endforeach
-                </div> --}}
                 <div class="row">
+                    @if($userId)
                     @foreach ($data as $order)
+                    @if ($order['order_id'] == $userId)
                         <div class="col-12 product_detail_3">
                             <div class="col-12 order_title" >
                                 <h5>{{ $order['status'] }}</h5>
@@ -122,9 +58,12 @@
                                     </div>
                                 </div>
                         </div>
+                    @endif
                     @endforeach
+                @else
+                <p>Bạn cần đăng nhập để xem đơn hàng.</p>
+                @endif
                 </div>
-              
             </div>
         </div>
     </div>
