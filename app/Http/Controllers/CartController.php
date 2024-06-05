c9064ed1c52fb3b097b3cf4b1833191c320bc66d
subinkhang <subinkhang102@gmail.com>
2024-06-06 01:58:19 +0700

    Resolve conflict after rebase

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -19,26 +19,25 @@ class CartController extends Controller
         return view('user/cart');
     }
     public function checkVoucher(Request $request)
-    {
-        $code = $request->input('code_voucher');
-        $voucher = DB::table('voucher')->where('code_voucher', $code)->first();
+{
+    $code = $request->input('code_voucher');
+    $voucher = DB::table('voucher')->where('code_voucher', $code)->first();
 
-        if (!$voucher) {
-            return response()->json(['error' => 'This voucher is not valid']);
-        }
-
-        $currentDate = Carbon::now();
-
-        if ($voucher->number_voucher <= 0 ||
-            $currentDate < $voucher->start_voucher ||
-            $currentDate > $voucher->end_voucher) {
-            return response()->json(['error' => 'This voucher is not valid']);
-        }
-
-        return response()->json([
-            'condition_voucher' => $voucher->condition_voucher,
-            'value_voucher' => $voucher->value_voucher
-        ]);
+    if (!$voucher) {
+        return response()->json(['error' => 'This voucher is not available']);
     }
+
+    $currentDate = Carbon::now();
+
+    if ($voucher->number_voucher <= 0 ||
+        $currentDate < $voucher->start_voucher ||
+        $currentDate > $voucher->end_voucher) {
+        return response()->json(['error' => 'This voucher is no longer valid']);
+    }
+
+    return response()->json([
+        'condition_voucher' => $voucher->condition_voucher,
+        'value_voucher' => $voucher->value_voucher
+    ]);
 }
 }
