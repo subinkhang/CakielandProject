c6f45daeb25b05c502c1f165be1add034101a23a
hanhuyen <daothihanhuyen10a3.19.20@gmail.com>
2024-06-05 16:27:14 +0700

    Fix Error and Icon eyes

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -14,7 +14,6 @@ new #[Layout('layouts.guest')] class extends Component
     public string $email = '';
     public string $password = '';
     public string $password_confirmation = '';
-    public string $currentError = '';
     // public $currentError = null;
     /**
      * Handle an incoming registration request.
@@ -26,32 +25,15 @@ new #[Layout('layouts.guest')] class extends Component
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
         ]);
-        $this->currentError = 'name';
-
         $validated['password'] = Hash::make($validated['password']);
-
-        // $this->showErrors = true;
-
         event(new Registered($user = User::create($validated)));
-        // $this->redirect()->intended(route('verify-email', absolute: false));
         Auth::login($user);
-        // $this->redirect('/login');
-        // $this->redirectIntended(default: route('verify-email', absolute: false), navigate: true);
-
         $this->redirect(route('verification.notice'), navigate: true);
 
         
     }
-    public function updated($field)
-{
-    if ($this->currentError === $field && $errors->has($field)) {
-        // Nếu lỗi hiện tại đã được sửa, chuyển sang lỗi tiếp theo
-        // Ví dụ: nếu $currentError là 'name' và lỗi 'name' đã được sửa, cập nhật $currentError thành 'email'
-        $this->currentError = 'email'; // Cập nhật thành trường nhập liệu tiếp theo cần kiểm tra
-    }
-}
-}; ?>
-
+}; 
+?>
 <div>
     <head>
         <meta charset="UTF-8">
@@ -61,6 +43,7 @@ new #[Layout('layouts.guest')] class extends Component
         <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.css') }}">
         <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
         <link rel="stylesheet" href="{{ asset('frontend/css/signup.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     </head>
     <form wire:submit="register">
         @csrf
@@ -85,40 +68,21 @@ new #[Layout('layouts.guest')] class extends Component
                             <div class="row">
                                 <div class="col-2"></div>
                                 <div class="col-8">
-                                    {{-- <input type="text" placeholder="Full name" class="box1signup"> --}}
-                                    {{-- <div> --}}
                                         <x-input-label for="name" :value="__()" />
                                         <x-text-input wire:model="name" id="name" class="box1signup" type="text" name="name" placeholder="Full name" required autofocus autocomplete="name" />
-                                        {{-- @if ($errors->has('name'))
-                                            <div class="mt-2 text-red-600">
-                                            @foreach ($errors->get('name') as $message)
-                                                <p>{{ $message }}</p>
-                                        @endforeach
-                                        </div>
-                                        @endif --}}
+                                        
                                     {{-- </div> --}}
                                 </div>
                                 <div class="col-2"></div>
                                 <div class="col-2"></div>
                                 <div class="col-8">
-                                    {{-- <input type="text" placeholder="Email/Phone number" class="box2signup"> --}}
-                                    {{-- <div class="mt-4"> --}}
                                         <x-input-label for="email" :value="__()" />
                                         <x-text-input wire:model="email" id="email" class="box2signup" type="email" name="email" placeholder="Email" required autocomplete="username" />
-                                        {{-- @if ($errors->has('email'))
-                                            <div class="mt-2 text-red-600">
-                                                @foreach ($errors->get('email') as $message)
-                                                    <p>{{ $message }}</p>
-                                                @endforeach
-                                            </div>
-                                        @endif --}}
-                                    {{-- </div> --}}
+                                       
                                 </div>
                                 <div class="col-2"></div>
                                 <div class="col-2"></div>
                                 <div class="col-6">
-                                    {{-- <input type="text" placeholder="Password" class="box3signup"> --}}
-                                    {{-- <div class="mt-4"> --}}
                                         <x-input-label for="password" :value="__()" />
                             
                                         <x-text-input wire:model="password" id="password" class="box3signup"
@@ -126,18 +90,10 @@ new #[Layout('layouts.guest')] class extends Component
                                                         name="password"
                                                         placeholder="Password"
                                                         required autocomplete="new-password" />
-                                        {{-- @if ($errors->has('password'))
-                                            <div class="mt-2 text-red-600">
-                                                @foreach ($errors->get('password') as $message)
-                                                    <p>{{ $message }}</p>
-                                                @endforeach
-                                            </div>
-                                        @endif --}}
                                         
-                                    {{-- </div> --}}
                                 </div>
                                 <div class="col-2 box_eye">
-                                    <i class="fa-regular fa-eye"onclick="togglePasswordVisibility(this)"></i></div>
+                                    <i class="fa-regular fa-eye" onclick="togglePasswordVisibility(this)"></i></div>
                                 <div class="col-2"></div>
                                 <div class="col-2"></div>
                                 <div class="col-6">
@@ -148,10 +104,22 @@ new #[Layout('layouts.guest')] class extends Component
                                         <x-text-input wire:model="password_confirmation" id="password_confirmation" class="box4signup"
                                                         type="password"
                                                         name="password_confirmation" placeholder="Confirm password" required autocomplete="new-password" />
-                                        <x-input-error :messages="$errors->get($currentError)" class="mt-2" />
-                                        {{-- <x-input-error :messages="$errors->get($currentError)" class="mt-2" />
-                                        <x-input-error :messages="$errors->get($currentError)" class="mt-2" />
-                                        <x-input-error :messages="$errors->get($currentError)" class="mt-2" /> --}}
+                                       
+                                            @foreach ($errors->get('name') as $message)
+                                            <div class="error-message">{{ $message }}</div>
+                                        @endforeach
+                                        
+                                        @foreach ($errors->get('email') as $message)
+                                            <div class="error-message">{{ $message }}</div>
+                                        @endforeach
+                                        
+                                        @foreach ($errors->get('password') as $message)
+                                            <div class="error-message">{{ $message }}</div>
+                                        @endforeach
+                                        
+                                        @foreach ($errors->get('password_confirmation') as $message)
+                                            <div class="error-message">{{ $message }}</div>
+                                        @endforeach
                                     {{-- </div> --}}
                                 </div>
                                
@@ -170,8 +138,7 @@ new #[Layout('layouts.guest')] class extends Component
                             <div class="row linksi">
                                 <div class="col-2"></div>
                                 <div class="col-8 linksi2">
-                                    {{-- <p>Have account?</p>
-                                    <p class="p1">Sign in now!</p> --}}
+                                    
                                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}" wire:navigate>
                                         {{ __('Have an account? Sign in now!') }}
                                     </a>
@@ -187,6 +154,6 @@ new #[Layout('layouts.guest')] class extends Component
                 </div>
             </div>
         </body>
-        {{-- <script src="{{asset('frontend/js/signup.js')}}"></script> --}}
+        <script src="{{asset('frontend/js/signup.js')}}"></script>
     </form>
 </div>
